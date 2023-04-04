f6347ca1b076fd4c17420d6cbe5b5da393a29b20
efg <abc@cba.com>
2023-04-04 05:17:07 +0330

    nvm

--- server_config_installation_script.sh
+++ server_config_installation_script.sh
@@ -6,10 +6,10 @@ export name="$(whoami)"
 putgitrepo() {
 	# Downloads a gitrepo $1 and places the files in $2 only overwriting conflicts
 	whiptail --infobox "Downloading and installing config files..." 7 60
-	[ -z "$3" ] && branch="master" || branch="$repobranch"
+	[ -z "$3" ] && branch="main" || branch="$repobranch"
 	dir=$(mktemp -d)
 	[ ! -d "$2" ] && mkdir -p "$2"
-	chown "$name":wheel "$dir" "$2"
+	chown "$name":sudo "$dir" "$2"
 	sudo -u "$name" git -C "$repodir" clone --depth 1 \
 		--single-branch --no-tags -q --recursive -b "$branch" \
 		--recurse-submodules "$1" "$dir"
